38dd0d1e7457eece87ae80bedf30a515eefcbc6e
apiwat mingmit <120304596+Maxma23891@users.noreply.github.com>
2025-07-24 14:08:18 +0700

    maxma

--- Class3.py
+++ Class3.py
@@ -1,19 +1,19 @@
-# hasRice = False
-# hasSpoon = True 
+hasRice = False
+hasSpoon = True 
 
 
-# if hasRice: 
-#     print( " มีข้าว ")
-#     if hasSpoon :
-#       print( "มีช้อน")
-#       print("OK")
-#     else:
-#         print ( "กินข้าวไม่ได้")
-# else:
-#      print("Can't eat")
-# boxloop = int(input("กรอก")); Sum = 0
-# for i in range(1,boxloop+1):
-#  print("รอบที่ ",i, "ผลรวมคือ",Sum +i) ;Sum += i
+if hasRice: 
+    print( " มีข้าว ")
+    if hasSpoon :
+      print( "มีช้อน")
+      print("OK")
+    else:
+        print ( "กินข้าวไม่ได้")
+else:
+     print("Can't eat")
+boxloop = int(input("กรอก")); Sum = 0
+for i in range(1,boxloop+1):
+ print("รอบที่ ",i, "ผลรวมคือ",Sum +i) ;Sum += i
 
 
 
